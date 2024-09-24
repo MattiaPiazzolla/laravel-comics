dd1797529507286f015f69449f95ad7e8bc60198
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-09-24 20:52:05 +0200

    [UPD] defined and imported a footer in partials

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,24 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DC comics</title>
+    <title>DC comics / {{ Route::currentRouteName() }}</title>
     @vite(['resources/js/app.js'])
 </head>
 
 <body>
     <header>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    @include('partials.header')
-
-                </div>
-            </div>
-        </div>
+        @include('partials.header')
     </header>
     <main>
         @yield('content')
     </main>
+    <footer>
+        @include('partials.footer')
+    </footer>
 </body>
 
 </html>
